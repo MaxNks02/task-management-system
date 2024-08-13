03c5e058b80600b2d83cac3f468db854b61e114c
MaxNks02 <maman.nks02@gmail.com>
2024-08-13 13:52:17 +0300

    miss @SpringBootTest

--- src/test/java/com/example/systemtaskmanagement/TaskServiceTest.java
+++ src/test/java/com/example/systemtaskmanagement/TaskServiceTest.java
@@ -17,6 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.access.AccessDeniedException;
 
 import java.util.Collections;
@@ -27,6 +28,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
 
